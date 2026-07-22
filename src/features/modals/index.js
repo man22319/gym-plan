f4351093b45f3447de11ac6c70d4b5ad143d6537
man22319 <yasser.9706@gmail.com>
2026-07-22 16:52:57 -0400

    chore: changed PR symbols

--- src/features/modals/index.js
+++ src/features/modals/index.js
@@ -126,9 +126,9 @@ export function openHistoryModal(exId) {
   let prHtml = '';
   if (pr.heaviestSet || pr.highestVolume) {
     const prItems = [];
-    if (pr.heaviestSet) prItems.push(`<div class="pr-item"><span class="pr-icon">PR</span><div><div class="pr-item-label">Heaviest Set</div><div class="pr-item-val">${pr.heaviestSet.w} lbs × ${pr.heaviestSet.r} reps <span class="pr-item-date">${formatDate(pr.heaviestSet.date)}</span></div></div></div>`);
-    if (pr.highestVolume) prItems.push(`<div class="pr-item"><span class="pr-icon">∑</span><div><div class="pr-item-label">Best Volume</div><div class="pr-item-val">${pr.highestVolume.volume.toLocaleString()} lbs <span class="pr-item-date">${formatDate(pr.highestVolume.date)}</span></div></div></div>`);
-    if (pr.mostReps) prItems.push(`<div class="pr-item"><span class="pr-icon">▲</span><div><div class="pr-item-label">Most Reps</div><div class="pr-item-val">${pr.mostReps.r} reps @ ${pr.mostReps.w} lbs <span class="pr-item-date">${formatDate(pr.mostReps.date)}</span></div></div></div>`);
+    if (pr.heaviestSet) prItems.push(`<div class="pr-item"><span class="pr-icon">MAX</span><div><div class="pr-item-label">Heaviest Set</div><div class="pr-item-val">${pr.heaviestSet.w} lbs × ${pr.heaviestSet.r} reps <span class="pr-item-date">${formatDate(pr.heaviestSet.date)}</span></div></div></div>`);
+    if (pr.highestVolume) prItems.push(`<div class="pr-item"><span class="pr-icon">VOL</span><div><div class="pr-item-label">Best Volume</div><div class="pr-item-val">${pr.highestVolume.volume.toLocaleString()} lbs <span class="pr-item-date">${formatDate(pr.highestVolume.date)}</span></div></div></div>`);
+    if (pr.mostReps) prItems.push(`<div class="pr-item"><span class="pr-icon">REP</span><div><div class="pr-item-label">Most Reps</div><div class="pr-item-val">${pr.mostReps.r} reps @ ${pr.mostReps.w} lbs <span class="pr-item-date">${formatDate(pr.mostReps.date)}</span></div></div></div>`);
     prHtml = `<div class="hist-pr-section"><div class="hist-section-label">PERSONAL RECORDS</div><div class="hist-pr-list">${prItems.join('')}</div></div>`;
   }
 
